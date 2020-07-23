961d3ea9f9995a2608fb73d88916d1842bc855c6
xhz <747367352@qq.com>
2020-07-23 19:37:06 +0800

    [alex xu] feature: refactor function of guess to return right xAxB

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -8,10 +8,28 @@ public class GuessNumberGame {
     }
 
     public String guess(String guessNumber) {
-        if (answer.equals(guessNumber)) {
-            return "4A0B";
-        } else {
-            return "0A0B";
+        int rightNumberCount = countRightNumber(guessNumber);
+        int rightPositionCount = countRightPositionNumber(guessNumber);
+        return rightPositionCount + "A" + (rightNumberCount - rightPositionCount) + "B";
+    }
+
+    private int countRightNumber(String guessNumber) {
+        int count = 0;
+        for (int i = 0; i < guessNumber.length(); i++) {
+            if (this.answer.indexOf(guessNumber.charAt(i)) != -1) {
+                count++;
+            }
         }
+        return count;
+    }
+
+    private int countRightPositionNumber(String guessNumber) {
+        int count = 0;
+        for (int i = 0; i < guessNumber.length(); i++) {
+            if (this.answer.charAt(i) == guessNumber.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
