9eb56082da0823dbf9eef7fe57682709a608569d
xhz <747367352@qq.com>
2020-07-23 20:36:22 +0800

    [alex xu] refactor: refactor isGuessNumberValid, make it check the guess number valid

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -38,6 +38,16 @@ public class GuessNumberGame {
     }
 
     public boolean isGuessNumberValid(String guessNumber) {
-        return guessNumber.length() == 4;
+        if (guessNumber.length() != 4) {
+            return false;
+        }
+        for (int i = 0; i < guessNumber.length(); i++) {
+            for (int j = i + 1; j < guessNumber.length(); j++) {
+                if (guessNumber.charAt(i) == guessNumber.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
