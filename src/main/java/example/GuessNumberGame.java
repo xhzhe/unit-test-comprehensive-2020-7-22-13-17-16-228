b2bc2b4ce582cdabc597ac9e599796a26f1d6695
alex <alex.h.z.xu@oocl.com>
2020-07-28 13:50:39 +0800

    [alex xu] refactor: extract "game over" to constant

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class GuessNumberGame implements Game {
     private static final int GAME_TIMES = 6;
+    public static final String GAME_OVER = "game over";
     private final GuessNumberGameCore guessNumberGameCore;
 
     GuessNumberGame(GuessNumberGameCore guessNumberGameCore) {
@@ -17,6 +18,6 @@ public class GuessNumberGame implements Game {
             String guessNumber = scanner.next();
             System.out.println(guessNumberGameCore.guess(guessNumber));
         }
-        System.out.println("game over");
+        System.out.println(GAME_OVER);
     }
 }
